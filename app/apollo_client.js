445aa24f8a0ee77731878ea1eea33f8802236690
Sk. Imtiaz Ahmed <excalliburbd@gmail.com>
2018-10-28 16:13:49 +0600

    "ejected" apollo boost

--- app/apollo_client.js
+++ app/apollo_client.js
@@ -1,12 +1,35 @@
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import { withClientState } from 'apollo-link-state';
+const { createUploadLink } = require('apollo-upload-client')
 
 import clientState from './client_state';
 
 const cache = new InMemoryCache();
 
-module.exports = new ApolloClient({
-  uri: "http://192.168.2.100:4000/graphql",
+const stateLink = withClientState({
   cache,
-  clientState,
+  ...clientState
+});
+
+module.exports = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    stateLink,
+    new createUploadLink({
+      uri: process.env.GRAPHQL_URI,
+      credentials: 'same-origin'
+    })
+  ]),
+  cache
 });
