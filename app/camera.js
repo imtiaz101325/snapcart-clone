aeb1a415420ab5ef60f11d40912b020bcb3e999f
Sk. Imtiaz Ahmed <excalliburbd@gmail.com>
2018-10-28 19:53:38 +0600

    implemented file upload mutation on client

--- app/camera.js
+++ app/camera.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { Camera, Permissions } from 'expo';
 import styled from 'styled-components/native';
+import { ReactNativeFile } from 'apollo-upload-client';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 
 import Container from './shared/container';
 
@@ -31,12 +34,20 @@ export default class CameraExample extends React.Component {
     this.setState({ hasCameraPermission: status === 'granted' });
   }
 
-  snap = async () => {
+  snap = async (singleUpload) => {
     if (this.camera) {
-      console.log(this.camera)
       try {
         let photo = await this.camera.current.takePictureAsync();
         console.log(photo)
+
+        const file = new ReactNativeFile({
+          uri: photo.uri,
+          name: 'some_name.jpg',
+          type: 'image/jpeg'
+        });
+
+        singleUpload(file);
+
       } catch(err) {
         console.log(err)
       }
@@ -44,6 +55,14 @@ export default class CameraExample extends React.Component {
   };
 
   render() {
+    const SINGLE_UPLOAD = gql`
+      mutation SingleUpload($file: Upload!) {
+        singleUpload(file: $file) {
+          filename
+        }
+      }
+    `;
+
     const { hasCameraPermission } = this.state;
     if (hasCameraPermission === null) {
       return <View />;
@@ -53,7 +72,14 @@ export default class CameraExample extends React.Component {
       return (
         <Container>
           <StyledCamera type={this.state.type} innerRef={this.camera}>
-            <StyledTouchableOpacity onPress={this.snap}></StyledTouchableOpacity>
+            <Mutation mutation={SINGLE_UPLOAD} >
+              {
+                (singleUpload, { loading, error }) =>
+                  loading ?
+                    <ActivityIndicator /> :
+                    <StyledTouchableOpacity onPress={() => this.snap(singleUpload)}></StyledTouchableOpacity>
+              }
+            </Mutation>
           </StyledCamera>
         </Container>
       );
