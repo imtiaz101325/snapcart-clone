4516ed5c671c5f6960cd31eef072dc1758627f4e
Sk. Imtiaz Ahmed <excalliburbd@gmail.com>
2018-10-30 15:45:52 +0600

    tested ocr works fine :tada:

--- consumer/app.py
+++ consumer/app.py
@@ -1,8 +1,15 @@
 import records
 import os
+from PIL import Image
+import pytesseract
 
 db = records.Database(os.environ.get('DATABASE_URI'));
 rows = db.query('select * from uploads')
 
-for row in rows:
-  print(row.userid, row.path)
+filename = rows[0].path.split('/')[-1]
+filepath = os.path.join(os.getcwd(), 'images', filename)
+
+img = Image.open(filepath)
+result = pytesseract.image_to_string(img, lang="eng")
+
+print(result)
