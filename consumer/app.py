2262cf53a8543d7cd9b75f95af8f407a4d031bbc
Sk. Imtiaz Ahmed <excalliburbd@gmail.com>
2018-11-01 01:20:24 +0600

    Small fix:
    Open CV was not working on the docker container. Had to add some binaries

--- consumer/app.py
+++ consumer/app.py
@@ -1,15 +1,15 @@
 import records
 import os
-from PIL import Image
+import cv2
 import pytesseract
 
-db = records.Database(os.environ.get('DATABASE_URI'));
+db = records.Database(os.environ.get('DATABASE_URI'))
 rows = db.query('select * from uploads')
 
 filename = rows[0].path.split('/')[-1]
 filepath = os.path.join(os.getcwd(), 'images', filename)
 
-img = Image.open(filepath)
+img = cv2.imread(filepath)
 result = pytesseract.image_to_string(img, lang="eng")
 
 print(result)
